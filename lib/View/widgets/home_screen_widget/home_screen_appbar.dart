f6aa5b88ddb72baa0ef2891b47f3a7b820dac56c
Rafi1702 <108681282+Rafi1702@users.noreply.github.com>
2023-05-18 15:12:28 +0700

    Menambahkan widget profile-tile dan search-bar

--- lib/View/widgets/home_screen_widget/home_screen_appbar.dart
+++ lib/View/widgets/home_screen_widget/home_screen_appbar.dart
@@ -5,6 +5,74 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(top:44.0),
+      child: Column(
+        children: [
+          //Profile Tile
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36.0),
+            child: Row(
+              children: [
+                const CircleAvatar(),
+                const SizedBox(width: 10),
+                const Text(
+                  'Welcome, Chandra',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+                ),
+                const Spacer(),
+                Image.asset(
+                  'assets/icon/ic_setting.png',
+                  width: 20.0,
+                  height: 20.0,
+                ),
+                const SizedBox(width: 10.0),
+                Image.asset(
+                  'assets/icon/ic_notification.png',
+                  width: 20.0,
+                  height: 20.0,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 36),
+
+          //Search Box
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 64.0),
+            child: SizedBox(
+              height: 36.0,
+              child: TextField(
+                readOnly: true,
+                onTap: (){},
+                decoration: InputDecoration(
+                  prefixIcon: Image.asset(
+                    'assets/icon/ic_search.png',
+                  ),
+                  // prefixIconConstraints: const BoxConstraints(maxWidth:20.0, maxHeight:20.0,),
+                  suffixIcon: Image.asset(
+                    'assets/icon/ic_filter.png',
+                  ),
+                  hintText: 'Search',
+                  contentPadding: const EdgeInsets.only(top:-10.0),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 0.5,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 0.5,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
