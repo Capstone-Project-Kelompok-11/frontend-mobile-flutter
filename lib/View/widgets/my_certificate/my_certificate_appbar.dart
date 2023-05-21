e36d1ce2e864c8f6cae58461f5e3560e0894718d
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-21 22:08:51 +0700

    slicing app my certificate appbar

--- lib/View/widgets/my_certificate/my_certificate_appbar.dart
+++ lib/View/widgets/my_certificate/my_certificate_appbar.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:lms_apps/View/screens/theme/theme.dart';
 
 class MyCertificateAppBar extends StatelessWidget {
   const MyCertificateAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Stack(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Image.asset(
+                'assets/icon/ic_arrow_left.png',
+                width: 14,
+                height: 17,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Center(
+                child: Text(
+                  'My Certificate',
+                  style: blackTextStyle.copyWith(
+                      fontSize: 25, fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 42),
+      ],
+    );
   }
 }
