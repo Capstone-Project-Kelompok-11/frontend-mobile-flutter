c748821255f0f655bd8c37adec1e8465d8c6a56e
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-21 20:55:20 +0700

    Nambah widget my course appbar sama widget my course body (initial commitnya aja)

---
+++ lib/View/widgets/my_course/my_course_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCourseAppBar extends StatelessWidget {
+  const MyCourseAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/View/widgets/my_course/my_course_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCourseBody extends StatelessWidget {
+  const MyCourseBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
