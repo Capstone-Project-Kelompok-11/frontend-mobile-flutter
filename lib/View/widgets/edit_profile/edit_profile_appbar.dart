16400a8b9fc405aca7aa83c8c82342bc276914d0
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-21 21:49:32 +0700

    Slicing app di edit profile appbar

--- lib/View/widgets/edit_profile/edit_profile_appbar.dart
+++ lib/View/widgets/edit_profile/edit_profile_appbar.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:lms_apps/View/screens/theme/theme.dart';
 
 class EditProfileAppBar extends StatelessWidget {
   const EditProfileAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Stack(
+          children: [
+            IconButton(
+              onPressed: () {
+                // Navigator.pop(context);
+              },
+              icon: Image.asset(
+                'assets/icon/ic_arrow_left.png',
+                width: 14,
+                height: 17,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Center(
+                child: Text(
+                  'Edit Profile',
+                  style: blackTextStyle.copyWith(
+                      fontSize: 25, fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        CircleAvatar(
+          radius: 80,
+          child: Image.asset('assets/images/img_profile.png'),
+        ),
+      ],
+    );
   }
 }
