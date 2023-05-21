e8963231197a9f6ecf111c62575a9f410236a035
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-21 21:56:13 +0700

    initial widgets buat screen my certificate

---
+++ lib/View/widgets/my_certificate/my_certificate_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCertificateAppBar extends StatelessWidget {
+  const MyCertificateAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/View/widgets/my_certificate/my_certificate_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCertificateBody extends StatelessWidget {
+  const MyCertificateBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
