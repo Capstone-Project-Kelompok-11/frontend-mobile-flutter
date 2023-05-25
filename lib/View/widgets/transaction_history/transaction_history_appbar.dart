8286ed264dff2b9db371e6eb96f8c0ea1c0755a4
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-25 21:22:32 +0700

    Slicing app bagian appbar di fitur transaction history

--- lib/View/widgets/transaction_history/transaction_history_appbar.dart
+++ lib/View/widgets/transaction_history/transaction_history_appbar.dart
@@ -5,6 +5,31 @@ class TransactionHistoryAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        height: 42,
+        child: SizedBox(
+          height: double.infinity,
+          child: Stack(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/icon/ic_arrow_left.png',
+                    width: 20,
+                    height: 20,
+                  )),
+              const Center(
+                child: Text(
+                  'Transaction History',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
