451a988d3b63481211b4623d6a90bc735af0dd06
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-25 21:12:30 +0700

    nambah fitur widget transaction history body

---
+++ lib/View/widgets/transaction_history/transaction_history_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryBody extends StatelessWidget {
+  const TransactionHistoryBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
