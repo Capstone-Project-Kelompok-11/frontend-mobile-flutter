db9b2d948c3a831448670a58f3bfd12b9b376e03
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-25 21:13:21 +0700

    nambahin widget appbar sama body ke dalam screen

--- lib/View/screens/transaction_history_screen.dart
+++ lib/View/screens/transaction_history_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lms_apps/View/widgets/transaction_history/transaction_history_appbar.dart';
+import 'package:lms_apps/View/widgets/transaction_history/transaction_history_body.dart';
 
 class TransactionHistoryScreen extends StatelessWidget {
   const TransactionHistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          TransactionHistoryAppBar(),
+          TransactionHistoryBody(),
+        ],
+      ),
+    );
   }
 }
