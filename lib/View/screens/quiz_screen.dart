96e12e4110b3e03cf50bcfd7efabee899c7bc6de
Derajat007 <125979077+Derajat007@users.noreply.github.com>
2023-05-28 23:34:31 +0700

    menambahkan button di quiz screen

--- lib/View/screens/quiz_screen.dart
+++ lib/View/screens/quiz_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms_apps/View/widgets/quiz/quiz_appbarr.dart';
+import 'package:lms_apps/View/widgets/quiz/quiz_body.dart';
+import 'package:lms_apps/View/widgets/quiz/quiz_button_widget.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({super.key});
@@ -8,9 +11,17 @@ class QuizScreen extends StatelessWidget {
     return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            QuizAppbar(),
+            SizedBox(
+              height: 29,
+            ),
+            QuizBody(),
+          ],
         ),
       ),
+      floatingActionButton: QuizButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
