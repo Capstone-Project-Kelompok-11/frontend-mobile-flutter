fef4e4bcfad5bb154e499f3f6341da37f2dfdcd4
Derajat007 <125979077+Derajat007@users.noreply.github.com>
2023-05-29 18:49:44 +0700

    modifikasi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lms_apps/View/screens/home_screen.dart';
 import 'package:lms_apps/View/screens/lessons_screen.dart';
+import 'package:lms_apps/View/screens/quiz_screen.dart';
 import 'package:lms_apps/View/screens/splash_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LessonsScreen(),
+      home: QuizScreen(),
     );
   }
 }
