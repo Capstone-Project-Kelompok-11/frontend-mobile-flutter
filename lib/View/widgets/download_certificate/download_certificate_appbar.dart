b845a4c7ec7375bd4d2ce9780582823904eaaa23
gilanggfzn <gilangm913@gmail.com>
2023-05-29 20:59:37 +0700

    setting Button

--- lib/View/widgets/download_certificate/download_certificate_appbar.dart
+++ lib/View/widgets/download_certificate/download_certificate_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lms_apps/View/screens/my_course_screen.dart';
 import 'package:lms_apps/View/screens/theme/theme.dart';
 
 class DownloadCertifAppbar extends StatelessWidget {
@@ -6,33 +7,34 @@ class DownloadCertifAppbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 30, left: 20),
-          child: Stack(
-            children: [
-              IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }, 
-                  icon: Image.asset('assets/icon/ic_arrow_left.png')),
-              Center(
-                child: Text(
-                  'Certificate',
-                  style: blackTextStyle.copyWith(
-                      fontWeight: semiBold, 
-                      fontSize: 17.28, 
-                      color: Colors.black),
+    return  SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Stack(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const MyCourseScreen()));
+                    }, 
+                    icon: Image.asset('assets/icon/ic_arrow_left.png'),),
+                Center(
+                  child: Text(
+                    'Certificate',
+                    style: blackTextStyle.copyWith(
+                        fontWeight: semiBold, 
+                        fontSize: 17.28, 
+                        color: Colors.black),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 19,),
-        const Text('Basic Microsoft Program'),
-        const SizedBox(height:45 ,)
-      ],
+          const SizedBox(height: 19,),
+          Text('Basic Microsoft Program',style: blackTextStyle.copyWith(fontSize: 15,fontWeight: regular),),
+        ],
+      ),
     );
   }
 }
