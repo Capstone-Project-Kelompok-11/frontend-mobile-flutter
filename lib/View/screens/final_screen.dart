6cfc8d1bbde80fbe541559ff882a0bf6653e2d49
Derajat007 <125979077+Derajat007@users.noreply.github.com>
2023-06-03 22:26:32 +0700

    menambahkan screen final task

---
+++ lib/View/screens/final_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FInaltask extends StatelessWidget {
+  const FInaltask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
