9dc91552937a48d8f5888e9a8a287700cd5cf462
Derajat007 <125979077+Derajat007@users.noreply.github.com>
2023-06-05 18:43:14 +0700

    gtw kenapa gak bisa checkout jadi bebas aja commitnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lms_apps/View/screens/final_screen.dart';
 import 'package:lms_apps/View/screens/splash_screen.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FInaltask(),
+      home: SplashScreen(),
     );
   }
 }
