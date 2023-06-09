5ad7c9b4058b6858dcc04192ba763e8b8629d2f6
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-06-09 14:34:41 +0700

    nambahin view model buat transaction history

---
+++ lib/ViewModels/transaction_history_view_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lms_apps/Models/transaction_history_response.dart';
+import 'package:lms_apps/Services/transaction_history_service.dart';
+
+class TransactionHistoryViewModel with ChangeNotifier {
+  List<Datum> _transactionHistory = [];
+  List<Datum> get transactionHistory => _transactionHistory;
+
+  getTransaction() async {
+    final result = await TransactionHistoryService().getUserCourse();
+    if (result.data!.isNotEmpty) {
+      _transactionHistory = result.data!;
+      notifyListeners();
+    } else {
+      _transactionHistory = [];
+      notifyListeners();
+    }
+    notifyListeners();
+  }
+
+  currencyFormat({required dynamic number, required int decimalDigit}) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp.',
+      decimalDigits: decimalDigit,
+    );
+    // notifyListeners();
+    return currencyFormatter.format(number);
+  }
+}
