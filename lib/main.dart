b6ece8b68608b14db09a77d82f367c733174d3d7
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-06-10 10:09:01 +0700

    my course belom selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lms_apps/ViewModels/edit_profile_view_model.dart';
 import 'package:lms_apps/View/screens/splash_screen.dart';
+import 'package:lms_apps/ViewModels/my_course_view_model.dart';
 import 'package:lms_apps/ViewModels/transaction_history_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,9 @@ void main() {
         ChangeNotifierProvider<TransactionHistoryViewModel>(
           create: (_) => TransactionHistoryViewModel(),
         ),
+        ChangeNotifierProvider<MyCourseViewModel>(
+          create: (_) => MyCourseViewModel(),
+        )
       ],
       child: const MyApp(),
     ),
