8019bf93d98be27075fedf61e6f8a5b2b4431813
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-06-13 16:55:17 +0700

    menambahkan image dan name berdasarkan API di bagian profile appbar

--- lib/View/screens/profile_screen.dart
+++ lib/View/screens/profile_screen.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:lms_apps/View/widgets/profile/profile_appbar.dart';
 import 'package:lms_apps/View/widgets/profile/profile_body.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
